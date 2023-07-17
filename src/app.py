709dc38239f4660c3f11b4cdb3317edaea171383
Soude640 <55102573+Soude640@users.noreply.github.com>
2023-07-17 17:41:31 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,58 +1,60 @@
 import logging.config
 import logging.handlers
+import os
+import time
+
+from do import do
+from flask import Flask, request
+from pyspark.ml.feature import RFormula
+from pyspark.sql import SparkSession
 
 logging.config.fileConfig("log_config.ini")
 logging.info('Starting app')
+
 # logging.getLogger("apscheduler.executors.default").setLevel(logging.WARNING)
 # logging.getLogger("apscheduler.scheduler").setLevel(logging.WARNING)
 
-import findspark
-from pyspark.ml.feature import RFormula
-
-findspark.init()
-
-import flask
-from pyspark.sql import SparkSession
-
-import itertools
-
-cont = itertools.count()
+WORKER_MEMORY_AMOUNTS = os.getenv("WORKER_MEMORY_AMOUNTS")
+print(os.getenv("WORKER_MEMORY_AMOUNTS"))
+print(os.getenv("WORKER_CPU_COUNTS"))
+WORKER_CPU_COUNTS = os.getenv("WORKER_CPU_COUNTS")
 
 
 def get_spark_session():
-    global cont
     return SparkSession. \
         builder. \
         master("spark://spark-master:7077"). \
-        appName(str(int(next(cont)) % 4)). \
-        config("spark.executor.memory", "14g"). \
-        config("spark.executor.cores", "4"). \
+        config("spark.executor.memory", WORKER_MEMORY_AMOUNTS). \
+        config("spark.executor.cores", WORKER_CPU_COUNTS). \
         getOrCreate()
 
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
 @app.route('/get/', methods=['GET'])
 def spark_test():
-    global registry, fail_response_count, success_response_count, process_time_summary
+    args = request.args
+    logging.info(
+        f'is_need_spark_session_stop: {args},'
+        f' {args.get("is_need_spark_session_stop")} {type(args.get("is_need_spark_session_stop"))}'
+    )
     spark = get_spark_session()
-    sample = None
+    if args.get('is_need_spark_session_stop') == "True":
+        spark.stop()
+        time.sleep(5)
+        spark = get_spark_session()
+        time.sleep(10)
+    logging.info(f'spark:  {spark}')
+    file_name = args.get("file_name")
     try:
-        path = f"/opt/workspace/data/g2.csv"
-        df = spark.read.csv(path, header=True)
-
-        formula = RFormula(
-            formula="clicked ~ country_code1 + hour",
-            featuresCol="features",
-            labelCol="label"
-        )
-        output = formula.fit(df).transform(df)
-        sample = output.select("features", "label").head(5)
-        return f"<h1>Sample</h1><p>{sample}</p>"
+        path = f"/opt/workspace/data/{file_name}"
+        logging.info(f'path {path} exists: {os.path.exists(path)}')
+        response = do(path, spark)
+        return f"<h1>Sample</h1><p>{response}</p>"
     except Exception as e:
-        return f"Some Exception Throwed : {sample} {e}"
+        return f"Some Exception Throwed: {e}"
 
 
 if __name__ == '__main__':
